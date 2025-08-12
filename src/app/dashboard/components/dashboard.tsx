8fecd3657794392ba80787f23f94a7bc05b6f842
Tom Keddie <thomaskeddie@gmail.com>
2025-08-12 15:42:01 +0000

    instead of drawing a tarot card, the dashboard will now show a card that

--- src/app/dashboard/components/dashboard.tsx
+++ src/app/dashboard/components/dashboard.tsx
@@ -1,25 +1,40 @@
 
+import { drawTarotCard } from '@/ai/flows/draw-tarot-card';
+import { generateAffirmation } from '@/ai/flows/generate-affirmation';
+import { getDailyAngelNumber } from '@/ai/flows/get-daily-angel-number';
+import { getAngelNumberMeaning } from '@/ai/flows/get-angel-number-meaning';
 import { getDailyReading } from '@/ai/flows/get-daily-reading';
 import { getMoonPhase } from '@/ai/flows/get-moon-phase';
 import { format } from 'date-fns';
 import { DailyReadingCard } from './daily-reading-card';
 import { TodaysMoonPhaseCard } from './todays-moon-phase-card';
-import { TarotCardCta } from './tarot-card-cta';
-import { AngelNumberCta } from './angel-number-cta';
-import { AffirmationCta } from './affirmation-cta';
+import { TarotCard } from './tarot-card';
+import { TodaysAffirmationCard } from './todays-affirmation-card';
+import { TodaysAngelNumberCard } from './todays-angel-number-card';
 
 export async function Dashboard() {
   const dailyReadingPromise = getDailyReading();
+  const todaysCardPromise = drawTarotCard();
+  const todaysAffirmationPromise = generateAffirmation({ category: 'confidence' });
+  const dailyAngelNumberPromise = getDailyAngelNumber();
   const moonPhasePromise = getMoonPhase({ date: format(new Date(), 'yyyy-MM-dd'), isNorthernHemisphere: true });
 
   const [
     dailyReading,
+    todaysCard,
+    todaysAffirmation,
+    dailyAngelNumberResponse,
     moonPhase,
   ] = await Promise.all([
     dailyReadingPromise,
+    todaysCardPromise,
+    todaysAffirmationPromise,
+    dailyAngelNumberPromise,
     moonPhasePromise,
   ]);
   
+  const todaysAngelNumberMeaning = await getAngelNumberMeaning({ number: dailyAngelNumberResponse.number, topic: 'General' });
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
       <div className="lg:col-span-2">
@@ -28,10 +43,10 @@ export async function Dashboard() {
       <div className="lg:col-span-2">
         <TodaysMoonPhaseCard moonPhase={moonPhase} />
       </div>
-      <TarotCardCta />
-      <AngelNumberCta />
+      <TarotCard card={todaysCard} />
+      <TodaysAngelNumberCard number={dailyAngelNumberResponse.number} meaning={todaysAngelNumberMeaning} />
       <div className="lg:col-span-2">
-        <AffirmationCta />
+        <TodaysAffirmationCard affirmation={todaysAffirmation.affirmation} />
       </div>
     </div>
   );
